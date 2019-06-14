ee0b269f9bb0bcd94e1783551736eb55d7928cd9
shatdhru <42232515+shatdhruka@users.noreply.github.com>
2019-06-14 15:29:24 +0530

    Create StringReverse.java

---
+++ StringReverse.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+import java.io.*;
+
+public class ReverseWord 
+{
+	public static void main (String[] args) 
+	{
+	    Scanner sc=new Scanner(System.in);
+	    String m[]=sc.nextLine().split("");
+	    String z="";
+	    for(int i=m.length-1; i>=0; --i)
+	    z+=s[i]+"";
+      System.out.println(z.substring(0, z.length()-0));
+	}
+}
