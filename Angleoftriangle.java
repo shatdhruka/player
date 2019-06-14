1c12fbdfd28d1364ae27c9387fe7ef0f50d6cadc
shatdhru <42232515+shatdhruka@users.noreply.github.com>
2019-06-14 15:00:51 +0530

    Update Angleoftriangle.java

--- Angleoftriangle.java
+++ Angleoftriangle.java
@@ -1,19 +1,20 @@
 import java.util.*;
+
 public class Angleoftriangle
 {
 	public static void main(String[] args)
 	{
 	    Scanner sc = new Scanner(System.in);
-        int a=sc.nextInt();
-        int b=sc.nextInt();
-        int c=sc.nextInt();
-        if(a!=0 && b!=0 && c!=0 && (a+b+c==180) )
-        {
-            System.out.println("yes");
-        }
-        else
-        {
-            System.out.println("no");
-        }
-    }
+            int x=sc.nextInt();
+            int y=sc.nextInt();
+            int z=sc.nextInt();
+            if(x!=0 && y!=0 && z!=0 && (x+y+z==180) )
+            {
+            	System.out.println("yes");
+            }
+            else
+            {
+            	System.out.println("no");
+            }
+    	}
 }
